68e63a00bfe4de098d6114fecab8d4153b6339d2
Tóth Viktor <50296138+vitya0717@users.noreply.github.com>
2023-03-16 19:30:51 +0100

    WEBSHOPNEMTOMJOE

--- database/WEBSHOP/adatok.sql
+++ database/WEBSHOP/adatok.sql
@@ -24,87 +24,110 @@ USE webshop;
 
 /*Táblák: beszállító, termék, felhasznalo, rendelés, raktár*/
 CREATE TABLE varos(
-    varos_azon INT AUTO_INCREMENT,
     iranyito_szam INT,
     varos_neve VARCHAR(64),
-    PRIMARY KEY(varos_azon)
+    PRIMARY KEY(iranyito_szam)
 );
 
 CREATE TABLE utca(
     utca_azon INT AUTO_INCREMENT,
     utca_neve VARCHAR(64),
-    hazszam INT,
     PRIMARY KEY(utca_azon)
+);
+
+CREATE TABLE bolt(
+    bolt_azon INT AUTO_INCREMENT,
+    bolt_neve VARCHAR(64),
+    PRIMARY KEY(bolt_azon)
 );
 
 CREATE TABLE felhasznalo(
     felhasz_azon INT AUTO_INCREMENT,
     varos_azon INT,
     utca_azon INT,
+    hazszam INT,
     felhasznev VARCHAR(64),
     jelszo VARCHAR(128),
     email VARCHAR(64),
-    INDEX(felhasz_azon),
+    PRIMARY KEY(felhasz_azon),
     INDEX(varos_azon),
     INDEX(utca_azon),
-    FOREIGN KEY(varos_azon) REFERENCES varos(varos_azon),
+    FOREIGN KEY(varos_azon) REFERENCES varos(iranyito_szam),
     FOREIGN KEY(utca_azon) REFERENCES utca(utca_azon)
 );
 
 CREATE TABLE beszallito(
-    beszallito_azon INT AUTO_INCREMENT,
+    bolt_azon INT,
     varos_azon INT,
     utca_azon INT,
     beszallito_telszam VARCHAR(64),
-    INDEX(beszallito_azon),
     INDEX(varos_azon),
     INDEX(utca_azon),
-    FOREIGN KEY(varos_azon) REFERENCES varos(varos_azon),
-    FOREIGN KEY(utca_azon) REFERENCES utca(utca_azon)
-);
-
-CREATE TABLE raktar(
-    raktar_azon INT AUTO_INCREMENT,
-    termek_mennyiseg INT,
-    INDEX(raktar_azon)
+    INDEX(bolt_azon),
+    FOREIGN KEY(varos_azon) REFERENCES varos(iranyito_szam),
+    FOREIGN KEY(utca_azon) REFERENCES utca(utca_azon),
+    FOREIGN KEY(bolt_azon) REFERENCES bolt(bolt_azon)
 );
 
 CREATE TABLE termek(
     termek_azon INT AUTO_INCREMENT,
-    raktar_azon INT,
     termek_neve VARCHAR(64),
     termek_ara INT,
-    INDEX(termek_azon),
-    INDEX(raktar_azon),
-    FOREIGN KEY(raktar_azon) REFERENCES raktar(raktar_azon)
+    raktar_elerhetoseg BOOLEAN,
+    raktar_termek_mennyiseg INT,
+    INDEX(termek_azon)
 );
 
 CREATE TABLE rendeles(
     rendeles_azon INT AUTO_INCREMENT,
+    bolt_azon INT,
     termek_azon INT,
-    beszallito_azon INT,
     felhasznalo_azon INT,
     rendeles_datuma DATE,
     fizetendo_osszeg INT,
     INDEX(termek_azon),
-    INDEX(beszallito_azon),
     INDEX(felhasznalo_azon),
+    INDEX(bolt_azon),
     PRIMARY KEY(rendeles_azon),
     FOREIGN KEY(termek_azon) REFERENCES termek(termek_azon),
-    FOREIGN KEY(beszallito_azon) REFERENCES beszallito(beszallito_azon),
+    FOREIGN KEY(bolt_azon) REFERENCES bolt(bolt_azon),
     FOREIGN KEY(felhasznalo_azon) REFERENCES felhasznalo(felhasz_azon)
 );
-INSERT INTO varos(varos_azon, iranyito_szam, varos_neve) VALUES 
-(1, "3500", "Miskolc"),
-(2, "1011", "Budapest"),
-(3, "2640", "Nógrád");
+INSERT INTO varos(iranyito_szam, varos_neve) VALUES 
+    (3500, "Miskolc"),
+    (1011, "Budapest"),
+    (2640, "Nógrád");
 
-INSERT INTO utca(utca_azon, utca_neve, hazszam) VALUES 
-(1,"Kis Pista", 43),
-(2, "Fiastyúk", 243),
-(3, "Terhes utca", 65);
+INSERT INTO utca(utca_azon, utca_neve) VALUES 
+    (1, "Kis Pista"),
+    (2, "Móra Ferenc"),
+    (3, "Kelepes utca");
 
-INSERT INTO felhasznalo(felhasz_azon, varos_azon, utca_azon, felhasznev, jelszo, email) VALUES 
-(1, 2, 2, "gerimester2009", "Alma12345X", "gerimester200@gmail.com"),
-(1, 1, 2, "yummmiiiOTP", "Jelszo", "lazlo.mail@freemail.com"),
-(1, 2, 2, "ZsalmaLeves", "Naananaaa23", "zalma.lev10@citromail.com");
+INSERT INTO bolt(bolt_azon, bolt_neve) VALUES 
+    (1, "Media Markt"),
+    (2, "Euronics"),
+    (3, "Emag"),
+    (4, "Ipon"),
+    (5, "PCX");
+
+INSERT INTO felhasznalo(felhasz_azon, varos_azon, utca_azon, hazszam, felhasznev, jelszo, email) VALUES 
+    (1, 3500, 1, 43, "gerimester2009", SHA2("Alma12345X", 256), "gerimester200@gmail.com"),
+    (2, 1011, 2, 34, "yummmiiiOTP", SHA2("Jelszo", 256), "lazlo.mail@freemail.com"),
+    (3, 2640, 3, 21, "KolomposRikardo", SHA2("Naananaaa23", 256), "zalma.lev10@citromail.com");
+
+INSERT INTO beszallito(bolt_azon, varos_azon, utca_azon, beszallito_telszam) VALUES 
+    (1, 1011, 3, "1(81)732-99-69"),
+    (5, 3500, 2, "18(907)185-01-03"),
+    (4, 2640, 1, "70(693)288-79-87");
+
+INSERT INTO termek(termek_azon, termek_neve, termek_ara, raktar_elerhetoseg, raktar_termek_mennyiseg) VALUES
+    (1, "Samsung Galaxy S21 Ultra", 399000, true, 50),
+    (2, "Apple MacBook Air (2021)", 599000, true, 12),
+    (3, "Asus ROG 64GB, 5TB SDD", 9000000, true, 1),
+    (4, "Samsung TV", 50000, false, 0),
+    (5, "Levono IDEJAPAD", 250000, true, 25);
+
+INSERT INTO rendeles(rendeles_azon, termek_azon, bolt_azon, felhasznalo_azon, rendeles_datuma, fizetendo_osszeg) VALUES 
+    (1, 4, 2, 3, "2023-03-12", 50000),
+    (2, 1, 3, 2, "2023-03-12", 250000),
+    (3, 3, 5, 1, "2023-01-24", 9000000);
