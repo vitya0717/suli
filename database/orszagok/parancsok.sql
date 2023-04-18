5f83b775e9c172be1e1286a11a0206df1d0e49e4
tothv1 <50296138+vitya0717@users.noreply.github.com>
2023-04-18 11:21:34 +0200

    weatherAPI

--- database/orszagok/parancsok.sql
+++ database/orszagok/parancsok.sql
@@ -1,3 +1,31 @@
+/*1. Mi MADAGASZKÁR fővárosa?*/
+select fovaros from `orszagok` where orszag="MADAGASZKÁR";
+/*2. Melyik ország fővárosa OUAGADOUGOU?*/
+select orszag from `orszagok` where fovaros="OUAGADOUGOU";
+/*3. Melyik ország autójele a TT?*/
+select orszag from `orszagok` where autojel="TT";
+/*4. Melyik ország pénzének jele az SGD?*/
+select orszag from `orszagok` where penzjel="SGD";
+/*5. Melyik ország nemzetközi telefon-hívószáma a 61?*/
+select orszag from `orszagok` where telefon=61;
+/*6. Mekkora területű Monaco?*/
+select terulet from `orszagok` where orszag="Monaco";
+/*7. Hányan laknak Máltán?*/
+select nepesseg*1000 from `orszagok` where orszag="MÁLTA";
+/*8. Mennyi Japán népsűrűsége?*/
+select (nepesseg/terulet)*1000 as `nepsuruseg` from `orszagok`  where orszag="JAPÁN";
+/*9. Hány lakosa van a Földnek?*/
+select sum(nepesseg)*1000 as `foldnepesseg` from `orszagok`;
+/*10. Mennyi az országok területe összesen?*/
+select sum(terulet) as `osszterulet` from `orszagok`;
+/*11. Mennyi az országok átlagos népessége?*/
+select avg(nepesseg)*1000 as `atlagnepesseg` from `orszagok`;
+/*12. Mennyi az országok átlagos területe?*/
+select avg(terulet) as `atlagnepesseg` from `orszagok`;
+/*13. Mennyi a Föld népsűrűsége?*/
+select sum(nepesseg*1000)/sum(terulet) as `nepsuruseg` from `orszagok`;
+/*14. Hány 1.000.000 km2-nél nagyobb területű ország van?*/
+select orszag as `kisebb` from `orszagok` where terulet>1000000; 
 /*15. Hány 100 km2-nél kisebb területű ország van?*/
 SELECT COUNT(*) FROM orszagok WHERE terulet < 100;
 /*16. Hány 20.000 főnél kevesebb lakosú ország van?*/
@@ -15,7 +43,7 @@ SELECT orszag, (SELECT SUM(nepesseg*1000) / SUM(terulet)) AS Nepsuruseg FROM ors
 /*22. Hány ország államformája köztársaság?*/
 SELECT COUNT(*) FROM orszagok WHERE allamforma  LIKE "%köztársaság%";
 /*23. Mely országok pénzneme a kelet-karib dollár?*/
-SELECT COUNT(*) FROM orszagok WHERE penznem = "kelet-karib dollár";
+SELECT orszag FROM orszagok WHERE penznem = "kelet-karib dollár";
 /*24. Hány ország nevében van benne az &quot;ORSZÁG&quot; szó?*/
 SELECT count(*) FROM orszagok WHERE orszag LIKE "%ORSZÁG%";
 /*25. Mely országokban korona a hivatalos fizetőeszköz?*/
